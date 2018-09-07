4dd0cc7e7872f6e8f8ca2efc7d16a53092049422
adaml <adam.lavie@gmail.com>
2018-09-07 21:02:17 +0300

    Add db transaction support and update some dependencies

--- rest_service/__init__.py
+++ rest_service/__init__.py
@@ -4,18 +4,22 @@ from jeeves_commons.utils import wait_for_port
 from jeeves_commons.constants import (POSTGRES_HOST_IP_ENV,
                                       POSTGRES_HOST_PORT_ENV,
                                       RABBITMQ_HOST_IP_ENV,
-                                      RABBITMQ_HOST_PORT_ENV)
+                                      RABBITMQ_HOST_PORT_ENV,
+                                      DEFAULT_BROKER_PORT,
+                                      DEFAULT_POSTGRES_PORT)
 
 print 'waiting for postgres DB service...'
 connected = wait_for_port(host=os.getenv(POSTGRES_HOST_IP_ENV, '172.17.0.2'),
-                          port=int(os.getenv(POSTGRES_HOST_PORT_ENV, 5432)),
+                          port=int(os.getenv(POSTGRES_HOST_PORT_ENV,
+                                             DEFAULT_POSTGRES_PORT)),
                           duration=30)
 if not connected:
     raise RuntimeError('failed waiting for postgres DB')
 
 print 'waiting for rabbitmq service...'
 connected = wait_for_port(host=os.getenv(RABBITMQ_HOST_IP_ENV, '172.17.0.3'),
-                          port=int(os.getenv(RABBITMQ_HOST_PORT_ENV, 5672)),
+                          port=int(os.getenv(RABBITMQ_HOST_PORT_ENV,
+                                             DEFAULT_BROKER_PORT)),
                           duration=30)
 if not connected:
     raise RuntimeError('failed waiting for rabbitmq broker')
