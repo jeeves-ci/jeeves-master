f37ae2bf12c3ac446a0c205dacee98261b314ca8
adaml <adam.lavie@gmail.com>
2018-09-07 23:08:25 +0300

    Apply wait for services on service startup. This is done to allow docker-compose support

--- rest_service/__init__.py
+++ rest_service/__init__.py
@@ -8,18 +8,35 @@ from jeeves_commons.constants import (POSTGRES_HOST_IP_ENV,
                                       DEFAULT_BROKER_PORT,
                                       DEFAULT_POSTGRES_PORT)
 
-print 'waiting for postgres DB service...'
-connected = wait_for_port(host=os.getenv(POSTGRES_HOST_IP_ENV, '172.17.0.2'),
-                          port=int(os.getenv(POSTGRES_HOST_PORT_ENV,
-                                             DEFAULT_POSTGRES_PORT)),
-                          duration=30)
-if not connected:
-    raise RuntimeError('failed waiting for postgres DB')
 
-print 'waiting for rabbitmq service...'
-connected = wait_for_port(host=os.getenv(RABBITMQ_HOST_IP_ENV, '172.17.0.3'),
-                          port=int(os.getenv(RABBITMQ_HOST_PORT_ENV,
-                                             DEFAULT_BROKER_PORT)),
-                          duration=30)
-if not connected:
-    raise RuntimeError('failed waiting for rabbitmq broker')
+try:
+    postgres_port = int(os.getenv(POSTGRES_HOST_PORT_ENV,
+                                  DEFAULT_POSTGRES_PORT))
+except ValueError:
+    postgres_port = DEFAULT_POSTGRES_PORT
+
+try:
+    rabbitmq_port = int(os.getenv(RABBITMQ_HOST_PORT_ENV,
+                                  DEFAULT_BROKER_PORT))
+except ValueError:
+    rabbitmq_port = DEFAULT_BROKER_PORT
+
+
+def wait():
+    print 'waiting for postgres DB service...'
+    connected = wait_for_port(host=os.getenv(POSTGRES_HOST_IP_ENV,
+                                             '172.17.0.2'),
+                              port=postgres_port,
+                              duration=30)
+    if not connected:
+        raise RuntimeError('failed waiting for postgres DB')
+
+    print 'waiting for rabbitmq service...'
+    connected = wait_for_port(host=os.getenv(RABBITMQ_HOST_IP_ENV,
+                                             '172.17.0.3'),
+                              port=rabbitmq_port,
+                              duration=30)
+    if not connected:
+        raise RuntimeError('failed waiting for rabbitmq broker')
+
+wait()
